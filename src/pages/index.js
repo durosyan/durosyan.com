fe7dd1d34329550d07f14fd01ce2522f4f02e8e3
durosyan <durosyan@outlook.com>
2020-10-20 21:16:09 +0100

    package setup

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,20 +1,21 @@
 import React from "react";
 import { Helmet } from "react-helmet";
+import CssBaseline from '@material-ui/core/CssBaseline';
+import Typography from '@material-ui/core/Typography';
+import Container from '@material-ui/core/Container';
 
-class Application extends React.Component {
-  render() {
-    return (
-      <div className="application">
-        <Helmet defer={false}>
-          <meta charSet="utf-8" />
-          <title>Durosyan</title>
-          <link rel="canonical" href="https://durosyan.com" />
-        </Helmet>
-      </div>
-    );
-  }
-};
-
-export default function Home() {
-  return <div>Hey!</div>
+export default function App() {
+  return (
+    <React.Fragment>
+      <Helmet defer={false}>
+        <meta charSet="utf-8" />
+        <title>Durosyan</title>
+        <link rel="canonical" href="https://durosyan.com" />
+      </Helmet>
+      <CssBaseline />
+      <Container maxWidth="sm">
+        <Typography component="div" style={{ backgroundColor: '#cfe8fc', height: '100vh' }} />
+      </Container>
+    </React.Fragment>
+  );
 }
