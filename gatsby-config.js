84c16496fae5db4e10ddb52930e06451ac3228c3
durosyan <durosyan@outlook.com>
2020-10-23 00:54:15 +0100

    added google analytics

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,25 @@ module.exports = {
           include: /assets/
         }
       }
-    }
+    },
+    {
+      resolve: `gatsby-plugin-google-analytics`,
+      options: {
+        // The property ID; the tracking code won't be generated without it
+        trackingId: "UA-180156318-1",
+        // Defines where to place the tracking script - `true` in the head and `false` in the body
+        head: false,
+        // Setting this parameter is optional
+        anonymize: true,
+        // Setting this parameter is also optional
+        respectDNT: true,
+        // Avoids sending pageview hits from custom paths
+        exclude: [],
+        // Delays sending pageview hits on route update (in milliseconds)
+        pageTransitionDelay: 0,
+        // Defers execution of google analytics script after page load
+        defer: false
+      },
+    },
   ],
 };
