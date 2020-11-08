79b118243ec89c086b961e6ebe704fc086cc1e3a
durosyan <durosyan@outlook.com>
2020-11-08 11:55:59 +0000

    added public folder for github.io

---
+++ public/component---src-pages-404-js-17786d7697802c778e5b.js
@@ -0,0 +1,2 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[3],{w2l6:function(e,n,t){"use strict";t.r(n),t.d(n,"default",(function(){return i}));var a=t("q1tI"),l=t.n(a),r=t("5CWz"),c=t("ofer"),o=t("Ji2X"),u=t("WDLW");function i(){return l.a.createElement(l.a.Fragment,null,l.a.createElement(u.a,null),l.a.createElement(r.a,null),l.a.createElement(o.a,null,l.a.createElement(c.a,{component:"div",style:{backgroundColor:"#212121"}},"404")))}}}]);
+//# sourceMappingURL=component---src-pages-404-js-17786d7697802c778e5b.js.map
